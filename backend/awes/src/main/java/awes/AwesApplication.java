ae1b8b0d724d2bb5db562cc97aa6b7f5ba9aae9c
yujin45 <yujin45@sookmyung.ac.kr>
2024-05-10 00:18:33 +0900

    [feat] add backend project

---
+++ backend/awes/src/main/java/awes/AwesApplication.java
@@ -0,0 +1,13 @@
+package awes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwesApplication.class, args);
+	}
+
+}
