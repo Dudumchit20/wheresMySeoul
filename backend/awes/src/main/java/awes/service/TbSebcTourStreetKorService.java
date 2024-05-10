25db62c0ebf4f4b4206da07f6caef9e6a25b7ce8
yujin45 <yujin45@sookmyung.ac.kr>
2024-05-10 18:26:15 +0900

    [test] some of searchOneCategory

--- backend/awes/src/main/java/awes/service/TbSebcTourStreetKorService.java
+++ backend/awes/src/main/java/awes/service/TbSebcTourStreetKorService.java
@@ -1,10 +1,13 @@
 package awes.service;
 
 import awes.entity.TbSebcTourStreetKor;
+import awes.entity.TbVwAttractions;
+import awes.entity.TbVwShopping;
 import awes.repository.TbSebcTourStreetKorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class TbSebcTourStreetKorService {
@@ -14,5 +17,12 @@ public class TbSebcTourStreetKorService {
 
     public List<TbSebcTourStreetKor> findAll() {
         return repository.findAll();
+    }
+
+    public List<TbSebcTourStreetKor> findByAddress(String address) {
+        List<TbSebcTourStreetKor> items = repository.findAll();
+        return items.stream()
+                .filter(item -> item.getLotAddress()!= null && item.getLotAddress().contains(address))
+                .collect(Collectors.toList());
     }
 }
