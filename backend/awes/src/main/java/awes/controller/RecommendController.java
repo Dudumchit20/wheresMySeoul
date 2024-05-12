cc31a99b28abf57a6f674bf5955ea1646748552b
yujin45 <yujin45@sookmyung.ac.kr>
2024-05-13 01:08:23 +0900

    [feat] add findTopNNearby streetKor table

--- backend/awes/src/main/java/awes/controller/RecommendController.java
+++ backend/awes/src/main/java/awes/controller/RecommendController.java
@@ -1,6 +1,9 @@
 package awes.controller;
 
+import awes.entity.TbSebcTourStreetKor;
+import awes.model.ResultRecommend;
 import awes.model.ResultRecommendTest;
+import awes.service.TbSebcTourStreetKorService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,18 +15,23 @@ import java.util.List;
 @RestController
 public class RecommendController {
 
+    private final TbSebcTourStreetKorService tbSebcTourStreetKorService;
     List<String> defaultCategory = Arrays.asList("관광거리", "명소", "문화", "쇼핑", "자연", "음식", "외국인");
     boolean numIsOverTen = false;
 
+    public RecommendController(TbSebcTourStreetKorService tbSebcTourStreetKorService) {
+        this.tbSebcTourStreetKorService = tbSebcTourStreetKorService;
+    }
+
     @GetMapping("/recommend")
-    public List<ResultRecommendTest> resultRecommendList(
+    public ResultRecommend resultRecommendList(
             @RequestParam(value = "address", defaultValue = "중구") String address,
-            @RequestParam(value="addressX", defaultValue = "0") double addressX,
-            @RequestParam(value="addressY", defaultValue = "0") double addressY,
+            @RequestParam(value="latitude", defaultValue = "0") double latitude,
+            @RequestParam(value="longitude", defaultValue = "0") double longitude,
             @RequestParam(value="num", defaultValue = "0") int num,
             @RequestParam(value="category") List<String> category
     ){
-        List<ResultRecommendTest> recommendList = new ArrayList<>();
+        ResultRecommend resultRecommend = new ResultRecommend();
 
         if (category == null || category.isEmpty()) {
             category = defaultCategory; // 기본 값 넣기
@@ -37,18 +45,62 @@ public class RecommendController {
 
         } else if (num<10) {
             // num이 10개 미만일 때
-            // 각 항목당 10개 뽑기
+            // 각 항목당 n개 뽑기
             numIsOverTen = false;
         }
 
         // category 항목 별 테이블 조회
         for(String categoryName : category) {
             // categoryName에 해당하는 테이블 조회
-            // select all로 전체 조회해서 가져오기
+            // n에 해당하는 대로
+            switch (categoryName) {
+                case "관광거리":
+                    // 관광거리 테이블 조회
+                    List<TbSebcTourStreetKor> tbSebcTourStreetKors = tbSebcTourStreetKorService.findNearby(latitude, longitude, num);
+                    resultRecommend.setTourStreetKor(tbSebcTourStreetKors);
+                    System.out.println(tbSebcTourStreetKors);
+                    // 조회된 결과를 처리하는 코드 작성
+                    break;
+                case "명소":
+                    // 명소 테이블 조회
+                    //List<TbVwSights> sights = sightsRepository.findAll();
+                    // 조회된 결과를 처리하는 코드 작성
+                    break;
+                case "문화":
+                    // 문화 테이블 조회
+                    //List<TbVwCulture> cultures = cultureRepository.findAll();
+                    // 조회된 결과를 처리하는 코드 작성
+                    break;
+                case "쇼핑":
+                    // 쇼핑 테이블 조회
+                    //List<TbVwShopping> shoppings = shoppingRepository.findAll();
+                    // 조회된 결과를 처리하는 코드 작성
+                    break;
+                case "자연":
+                    // 자연 테이블 조회
+                    //List<TbVwNature> natures = natureRepository.findAll();
+                    // 조회된 결과를 처리하는 코드 작성
+                    break;
+                case "음식":
+                    // 음식 테이블 조회
+                    //List<TbVwFood> foods = foodRepository.findAll();
+                    // 조회된 결과를 처리하는 코드 작성
+                    break;
+                case "외국인":
+                    // 외국인 테이블 조회
+                    //List<TbVwForeigners> foreigners = foreignersRepository.findAll();
+                    // 조회된 결과를 처리하는 코드 작성
+                    break;
+                default:
+                    // 처리할 categoryName이 없는 경우
+                    // 예외 처리 또는 기본 동작을 수행하는 코드 작성
+                    System.out.println("해당되는 항목 없음");
+                    break;
+            }
 
         }
 
-        // 받아온 위도X, 경도Y
+        // 받아온 위도Y, 경도X
         /*
         장소의 갯수 : 4개 선택
         원하는 분류 : 명소, 문화, 쇼핑
@@ -65,9 +117,10 @@ public class RecommendController {
         * */
 
 
-        return recommendList;
+        return resultRecommend;
     }
 
+    //// 테스트용이었던 것
     @GetMapping("/recommendLocation")
     public List<ResultRecommendTest> recommendLocation(
             @RequestParam(value = "address") String address,
